af13d9e6794fbdd53e1c820e3aa486be947920b7
Armand <armand@synaptent.com>
2026-04-20 13:12:29 -0500

    fix(mypy): avoid breakpoint import no-redef

--- aragora/server/handlers/breakpoints.py
+++ aragora/server/handlers/breakpoints.py
@@ -38,19 +38,17 @@ from .utils.rate_limit import RateLimiter, get_client_ip
 # Rate limiter for breakpoints endpoints (60 requests per minute - debug feature)
 _breakpoints_limiter = RateLimiter(requests_per_minute=60)
 
-_ImportedHumanGuidance: Any = None
-_ImportedBreakpointManager: Any = None
+_breakpoints_module: Any = None
 
 try:
-    from aragora.debate.breakpoints import (
-        BreakpointManager as _ImportedBreakpointManager,  # noqa: F811
-        HumanGuidance as _ImportedHumanGuidance,  # noqa: F811
-    )
+    from aragora.debate import breakpoints as _loaded_breakpoints_module
 except ImportError:
     pass
+else:
+    _breakpoints_module = _loaded_breakpoints_module
 
-HumanGuidance: Any = _ImportedHumanGuidance
-BreakpointManager: Any = _ImportedBreakpointManager
+HumanGuidance: Any = getattr(_breakpoints_module, "HumanGuidance", None)
+BreakpointManager: Any = getattr(_breakpoints_module, "BreakpointManager", None)
 
 
 class BreakpointsHandler(BaseHandler):
